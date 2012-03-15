93667d9deeaf8f8d1b1977981ff3922144735804
Igor Zubkov <igor.zubkov@gmail.com>
2012-03-15 13:04:13 +0200

    Init

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_test_collab do
+  name 'Redmine Test Collab plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
