38c93405b79bd00b5892e229b3f04880b1be3cc3
Igor Zubkov <igor.zubkov@gmail.com>
2012-03-15 13:29:37 +0200

    Add Configure link

--- init.rb
+++ init.rb
@@ -7,6 +7,10 @@ Redmine::Plugin.register :redmine_test_collab do
   version '0.0.1'
   url 'https://github.com/biow0lf/redmine_test_collab'
   author_url 'https://github.com/biow0lf'
+
+  settings(:default => {
+             'enabled' => false
+           }, :partial => 'settings/test_collab')
 end
 
 require 'redmine_test_collab/hooks/view_layouts_base_html_head_hook'
