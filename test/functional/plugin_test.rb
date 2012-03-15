f63d28878949df7dd4c2d76e175a9bcb3e896ab8
Igor Zubkov <igor.zubkov@gmail.com>
2012-03-15 16:18:52 +0200

    Tests

---
+++ test/functional/plugin_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  def setup
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+  end
+
+  def test_with_enabled_plugin
+    Setting['plugin_redmine_test_collab'][:enabled] = 'true'
+    get :index
+    assert @response.body.include?('<!-- HELLO -->')
+  end
+
+  def test_with_disabled_plugin
+    Setting['plugin_redmine_test_collab'][:enabled] = 'false'
+    get :index
+    assert !@response.body.include?('<!-- HELLO -->')
+  end
+end
