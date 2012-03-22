2b5b8aa3375a03ecb9bc31ffea6ee0d355e4529b
Igor Zubkov <igor.zubkov@gmail.com>
2012-03-22 11:55:27 +0200

    Move code from view

--- lib/redmine_test_collab/hooks/view_layouts_base_html_head_hook.rb
+++ lib/redmine_test_collab/hooks/view_layouts_base_html_head_hook.rb
@@ -1,10 +1,12 @@
 module RedmineTestCollab
   class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = { })
-      context[:controller].send(:render_to_string, {
-        :partial => 'test_collab/test_collab_js',
-        :locals => context
-      })
+      if Setting['plugin_redmine_test_collab'][:enabled] == 'true'
+        context[:controller].send(:render_to_string, {
+          :partial => 'test_collab/test_collab_js',
+          :locals => context
+        })
+      end
     end
   end
 end
